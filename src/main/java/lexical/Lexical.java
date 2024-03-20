260afaaf6d0446a3fda39f14b851aec99b84a140
amag1 <amaglione2002@gmail.com>
2024-03-20 15:43:20 -0300

    Add javadocs

--- src/main/java/lexical/Lexical.java
+++ src/main/java/lexical/Lexical.java
@@ -2,8 +2,13 @@ package lexical;
 
 import exceptions.lexical.LexicalException;
 
+/**
+ * Interfaz para analizador sintáctico con métodos para obtener el siguiente token
+ */
 public interface Lexical {
     public Token nextToken() throws LexicalException;
+
     public int getColumn();
+
     public int getLine();
 }
