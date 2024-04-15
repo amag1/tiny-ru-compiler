ed55cd0d1fe97d83f00c75a5a99259073b8667b1
amag1 <amaglione2002@gmail.com>
2024-04-14 21:02:22 -0300

    Add javadocs

--- src/main/java/syntactic/Syntactic.java
+++ src/main/java/syntactic/Syntactic.java
@@ -3,6 +3,9 @@ package syntactic;
 import exceptions.lexical.LexicalException;
 import exceptions.syntactic.SyntacticException;
 
+/**
+ * Interfaz para analizadores sintácticos.
+ */
 public interface Syntactic {
     void analyze() throws LexicalException, SyntacticException;
 }
