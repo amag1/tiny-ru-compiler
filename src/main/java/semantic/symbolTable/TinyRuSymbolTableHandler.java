307be0ac6849bb855104bc3613146035b46ac2d3
amag1 <amaglione2002@gmail.com>
2024-04-30 22:47:11 -0300

    Fix heredar de tipo primitivo

--- src/main/java/semantic/symbolTable/TinyRuSymbolTableHandler.java
+++ src/main/java/semantic/symbolTable/TinyRuSymbolTableHandler.java
@@ -103,7 +103,13 @@ public class TinyRuSymbolTableHandler implements SymbolTableHandler {
     }
 
     private boolean isValidInheritance(AttributeType type) {
-        return !type.isArray() && !type.isPrimitive();
+        if (type.isArray() || type.isPrimitive()) {
+            return false;
+        }
+
+        // Check if its a predefined class
+        ClassEntry predefinedClass = this.st.getClassByName(type.getType());
+        return predefinedClass == null || !predefinedClass.isPredefined();
     }
 
     /**
