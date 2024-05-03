87010edcce04a244887801eb3ce0f8fdeec4f637
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-02 22:58:57 -0300

    add attributes and methods

--- src/main/java/semantic/abstractSintaxTree/SentenceNode.java
+++ src/main/java/semantic/abstractSintaxTree/SentenceNode.java
@@ -1,4 +1,7 @@
 package semantic.abstractSintaxTree;
 
+import exceptions.semantic.SemanticException;
+
 public abstract class SentenceNode extends  AbstractSyntaxNode {
+    public abstract void validate() throws SemanticException;
 }
