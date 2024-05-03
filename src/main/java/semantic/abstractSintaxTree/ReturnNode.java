2084a07aa6f796643c36f8d2dfce69fc997dcffe
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-02 23:03:01 -0300

    add sentences

---
+++ src/main/java/semantic/abstractSintaxTree/ReturnNode.java
@@ -0,0 +1,18 @@
+package semantic.abstractSintaxTree;
+
+import exceptions.semantic.SemanticException;
+
+public class ReturnNode extends SentenceNode{
+    // TODO
+
+
+    @Override
+    public void validate() throws SemanticException {
+        // TODO
+    }
+
+    public String toJson(int indentationIndex) {
+        // TODO
+        return  "";
+    }
+}
