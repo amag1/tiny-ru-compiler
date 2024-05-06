83cde49336010b8a7a8b38a298b464aa455093b9
amag1 <amaglione2002@gmail.com>
2024-05-06 17:31:57 -0300

    Add field to distinguish between self and variable access

--- src/main/java/semantic/abstractSintaxTree/Expression/VariableAccessNode.java
+++ src/main/java/semantic/abstractSintaxTree/Expression/VariableAccessNode.java
@@ -6,22 +6,24 @@ import lexical.Type;
 import location.Location;
 import semantic.symbolTable.AttributeType;
 
-public class VariableAccessNode extends PrimaryNode{
+public class VariableAccessNode extends PrimaryNode {
     private String variableName;
+    private boolean isSelf;
 
-    public VariableAccessNode(String variableName) {
+    public VariableAccessNode(String variableName, boolean isSelf) {
         this.variableName = variableName;
+        this.isSelf = isSelf;
     }
 
 
     @Override
     public AttributeType getAttributeType() throws SemanticException {
         // TODO
-        return  new AttributeType(true, true, new Token("", Type.KW_IF, new Location()));
+        return new AttributeType(true, true, new Token("", Type.KW_IF, new Location()));
     }
 
     public String toJson(int indentationIndex) {
         // TODO
-        return  "";
+        return "";
     }
 }
