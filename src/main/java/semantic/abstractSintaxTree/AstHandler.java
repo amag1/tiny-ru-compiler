ee84f774ec46e85661d45db905f5c9ee81beb207
amag1 <amaglione2002@gmail.com>
2024-05-06 17:31:24 -0300

    Add methods to interface

--- src/main/java/semantic/abstractSintaxTree/AstHandler.java
+++ src/main/java/semantic/abstractSintaxTree/AstHandler.java
@@ -2,16 +2,19 @@ package semantic.abstractSintaxTree;
 
 import exceptions.semantic.SemanticException;
 import lexical.Token;
+import semantic.abstractSintaxTree.Expression.ArrayAccessNode;
 import semantic.abstractSintaxTree.Expression.LiteralNode;
 import semantic.abstractSintaxTree.Expression.PrimaryNode;
 import semantic.abstractSintaxTree.Expression.VariableAccessNode;
 
 public interface AstHandler {
-    public  void validateSenteces() throws SemanticException;
+    public void validateSenteces() throws SemanticException;
 
-    public LiteralNode createLiteral(Token token);
+    LiteralNode createLiteral(Token token);
 
-    public VariableAccessNode createVariableAccess(Token token);
+    VariableAccessNode createVariableAccess(Token token);
 
-    public PrimaryNode handlePossibleChain(PrimaryNode parentNode, PrimaryNode childrenNode);
+    ArrayAccessNode createArrayAccess(Token token);
+
+    PrimaryNode handlePossibleChain(PrimaryNode parentNode, PrimaryNode childrenNode);
 }
