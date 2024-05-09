f3e90e7eb3d2aa5a974daccaf980b50c105ba6b2
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-09 00:00:34 -0300

    fix empty sentence

--- src/main/java/semantic/abstractSintaxTree/Sentence/EmptySentenceNode.java
+++ src/main/java/semantic/abstractSintaxTree/Sentence/EmptySentenceNode.java
@@ -1,5 +1,7 @@
 package semantic.abstractSintaxTree.Sentence;
 
+import semantic.JsonHelper;
+
 public class EmptySentenceNode extends SentenceNode {
 
     public EmptySentenceNode() {
@@ -12,6 +14,9 @@ public class EmptySentenceNode extends SentenceNode {
     }
 
     public String toJson(int indentationIndex) {
-        return "";
+        indentationIndex++;
+        return "{" +
+                JsonHelper.json("nodeType", nodeType, indentationIndex) +
+                "\n" + JsonHelper.getIdentationString(indentationIndex - 1) + "}";
     }
 }
