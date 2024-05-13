67df30c64e29f701e599fb9382716a210d582c95
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-13 15:58:52 -0300

    fix UnaccesibleVariableException message

--- src/main/java/exceptions/semantic/syntaxTree/UnaccesibleVariableException.java
+++ src/main/java/exceptions/semantic/syntaxTree/UnaccesibleVariableException.java
@@ -4,6 +4,6 @@ import lexical.Token;
 
 public class UnaccesibleVariableException extends AstException {
     public UnaccesibleVariableException(Token token) {
-        super("La variable `" + token.getLexem() + "` es privada en la superclase, y, por lo tanto, inaccesible", token.getLocation());
+        super("La variable `" + token.getLexem() + "` es privada, y, por lo tanto, inaccesible", token.getLocation());
     }
 }
