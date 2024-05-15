fd43c7df569d6622def0b16ce10c18b1cdd59d13
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-15 16:56:57 -0300

    fix typo

--- src/main/java/exceptions/semantic/syntaxTree/InvalidMethodReturn.java
+++ src/main/java/exceptions/semantic/syntaxTree/InvalidMethodReturn.java
@@ -4,6 +4,6 @@ import lexical.Token;
 
 public class InvalidMethodReturn extends AstException{
     public InvalidMethodReturn(String expectedType, String foundType, Token token) {
-        super("Método retorna tipo " + expectedType + " pero se encontŕo " + foundType , token.getLocation());
+        super("Método retorna tipo " + expectedType + " pero se encontró " + foundType , token.getLocation());
     }
 }
