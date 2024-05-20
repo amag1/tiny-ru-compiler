bff9de24960718df21b5d20236b34942241ba757
Nahuel Arrieta <nahuel.arrietamza@gmail.com>
2024-05-20 00:19:52 -0300

    add passing test checcking

--- src/test/java/semantic/AstTest.java
+++ src/test/java/semantic/AstTest.java
@@ -1,8 +1,11 @@
 package semantic;
 
+import exceptions.lexical.LexicalException;
 import exceptions.semantic.symbolTable.SymbolTableException;
 import exceptions.semantic.syntaxTree.AstException;
+import exceptions.syntactic.SyntacticException;
 import lexical.LexicalAnalyzer;
+import logger.ConsoleLogger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -10,12 +13,15 @@ import org.junit.jupiter.params.provider.MethodSource;
 import reader.FileReader;
 import semantic.abstractSintaxTree.DummyAstHandler;
 import semantic.abstractSintaxTree.TinyRuAstHandler;
+import semantic.symbolTable.SymbolTable;
 import semantic.symbolTable.SymbolTableHandler;
 import semantic.symbolTable.TinyRuSymbolTableHandler;
 import syntactic.Syntactic;
 import syntactic.SyntacticAnalyzer;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.stream.Stream;
@@ -73,6 +79,38 @@ public class AstTest {
             assertEquals(e.getClass().getSimpleName(), errorName, "Error en el archivo: " + input);
         } catch (Exception e) {
             fail("Unexpected exception in " + input + ": " + e.getMessage());
+        }
+    }
+
+    private static Stream<Arguments> providePassingAst() {
+        String basepath = "src/main/java/semantic/abstractSintaxTree/test/passing";
+        return provider(basepath);
+    }
+
+    @ParameterizedTest
+    @MethodSource("providePassingAst")
+    public void TestPassingSymbolTable(String input, String fileOutput) {
+        try {
+            SymbolTableHandler stHandler = new TinyRuSymbolTableHandler();
+            Syntactic syntactic = new SyntacticAnalyzer(new LexicalAnalyzer(new FileReader(input)), stHandler, new TinyRuAstHandler(stHandler.getSymbolTableLookup()));
+            syntactic.analyze();
+        } catch (AstException e) {
+            ConsoleLogger clog = new ConsoleLogger();
+            clog.LogAstError(e);
+            fail();
+        } catch (IOException e) {
+            // If the file does not exist, simply check if no errors are thrown
+            try {
+                Syntactic syntactic = new SyntacticAnalyzer(new LexicalAnalyzer(new FileReader(input)), new TinyRuSymbolTableHandler() ,new DummyAstHandler());
+                syntactic.analyze();
+            } catch (SyntacticException | LexicalException | AstException |
+                     SymbolTableException ex) {
+                fail("Error en el archivo: " + input + "\n" + ex.getMessage());
+            } catch (FileNotFoundException ex) {
+                fail("Archivo no encontrado: " + input);
+            }
+        } catch (Exception e) {
+            fail("Error en el archivo: " + input + "\n" + e.getMessage());
         }
     }
 }
